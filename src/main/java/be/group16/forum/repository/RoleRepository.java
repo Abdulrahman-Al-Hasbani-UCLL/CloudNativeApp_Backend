bb6be8b7dc6a2e85309206a7e56b34c38a4bafae
Abdulrahman-Al-Hasbani-UCLL <Abdulrahman.alhasbani@student.ucll.be>
2025-04-29 16:52:14 +0200

    backend works

--- src/main/java/be/group16/forum/repository/RoleRepository.java
+++ src/main/java/be/group16/forum/repository/RoleRepository.java
@@ -2,11 +2,11 @@ package be.group16.forum.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import be.group16.forum.model.Role;
 
-public interface RoleRepository extends CrudRepository<Role, String> {
+public interface RoleRepository extends MongoRepository<Role, String> {
 
     Optional<Role> findByName(String roleName);
 
