fdfc19eb045ca0857306e87d4c3380905fef1175
Abdulrahman-Al-Hasbani-UCLL <abdulrahman.alhasbani@student.ucll.be>
2025-05-25 11:01:27 +0200

    Redis works locally

--- src/main/java/be/group16/forum/ForumApplication.java
+++ src/main/java/be/group16/forum/ForumApplication.java
@@ -2,9 +2,11 @@ package be.group16.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
+@EnableCaching
 public class ForumApplication {
 
     public static void main(String[] args) {
